8d6330b75cbfc71446e3f4c87ddc7ea6b7be21e7
YasminEstacio <yasmin.almeida238@gmail.com>
2026-01-26 20:37:44 +0000

    Nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
-  printf("Desafio Super Trunfo!\n");
-  printf("Novo commit\n");
+int População;
+float Área;
+float Pib;
+int ponto_turisticos; 
+
+printf("Digite o numero da população: \n");
+scanf("%d",&População);
+
+printf("Digite a Área: \n");
+scanf("%f", &Área);
+
+printf("Digite o Pib: \n");
+scanf("%f",&Pib);
+
+printf("Digite o Número de pontos turisticos: \n");
+scanf("%d",&ponto_turisticos);
+
+printf("População: %d - Área: %f\n", População, Área);
+printf("Pib: %f - ponto_turisticos: %d\n", Pib, ponto_turisticos);
+
+return 0;
 } 
