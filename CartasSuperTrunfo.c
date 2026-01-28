25b345e983872c064ce6681ec37cc34331fe28b6
YasminEstacio <yasmin.almeida238@gmail.com>
2026-01-28 16:41:36 +0000

    Desafio 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
 int main() {
+char Estado[50];
+char Cidade[50];
+char Codigo[50];
 int População;
 float Área;
 float Pib;
 int ponto_turisticos; 
 
-printf("Digite o numero da população: \n");
+printf("Estado: \n");
+scanf("%s",&Estado);
+
+printf("Cidade: \n");
+scanf("%s",&Cidade);
+
+printf("Codigo: \n");
+scanf("%s",&Codigo);
+
+printf("População: \n");
 scanf("%d",&População);
 
 printf("Digite a Área: \n");
-scanf("%f", &Área);
+scanf("%f",&Área);
 
 printf("Digite o Pib: \n");
 scanf("%f",&Pib);
@@ -18,6 +30,9 @@ scanf("%f",&Pib);
 printf("Digite o Número de pontos turisticos: \n");
 scanf("%d",&ponto_turisticos);
 
+printf("Estado: %s\n", Estado);
+printf("Cidade: %s\n", Cidade);
+printf("Codigo: %s\n", Codigo);
 printf("População: %d - Área: %f\n", População, Área);
 printf("Pib: %f - ponto_turisticos: %d\n", Pib, ponto_turisticos);
 
